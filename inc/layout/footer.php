6cb54749a99d43f5c5a416ee9ef8820f63c07e86
Remy <remy@colorz.fr>
2012-06-20 17:01:58 +0200

    setup

---
+++ inc/layout/footer.php
@@ -0,0 +1,6 @@
+        <footer class="footer">
+            <?php include TEMPLATEPATH.'/inc/modules/footer/'; ?>
+        </footer>
+    </div><?php //<!-- .wrapper --> ?>
+</body>
+</html>
---
+++ inc/layout/header.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="fr-FR" class="no-js">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>title</title>
+    <link rel="icon" href="/favicon.png">
+    <link rel="stylesheet" href="assets/style.php" type="text/css" />
+    <script type="text/javascript" src="assets/js/events.js"></script>
+    <!--[if lt IE 9]>
+        <script src="assets/js/ie/html5.js"></script>
+        <script src="assets/js/ie/selectivizr-1.0.2.min.js"></script>
+    <![endif]-->
+</head>
+<!--[if lt IE 7 ]><body is_ie6 lt_ie7 lt_ie8 lt_ie9 lt_ie10'); ?>><![endif]-->
+   <!--[if IE 7 ]><body is_ie7 lt_ie8 lt_ie9 lt_ie10'); ?>><![endif]-->
+   <!--[if IE 8 ]><body is_ie8 lt_ie9 lt_ie10'); ?>><![endif]-->
+   <!--[if IE 9 ]><body is_ie9 lt_ie10'); ?>><![endif]-->
+<!--[if gt IE 9 ]><body is_ie10 gt_ie9'); ?>><![endif]-->
+<!--[if !IE]><!--><body><!--<![endif]-->
+    <div class="wrapper">
+        <header class="header">
+            <?php include TEMPLATEPATH.'/inc/modules/header/'; ?>
+        </header>
