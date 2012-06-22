9dcd2df8b5237d9dfa746115b7a526158046c6e1
Remy <remy@colorz.fr>
2012-06-22 11:47:34 +0200

    add scroll with mootools

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,3 +1,22 @@
-hljs.initHighlightingOnLoad();
-// $(document).ready(function() {
-// });
+
+
+window.addEvent('domready', function(){
+
+    // Highlight
+    hljs.initHighlightingOnLoad();
+
+    // Smooth scroll
+    var scrollAnchor = new Fx.Scroll(window);
+
+    $$('.scrollAnchor').addEvent('click', function(e){
+        e.preventDefault();
+
+        var href = e.target.getProperty('href');
+        var anchor = href.split('#')[1];
+
+        scrollAnchor.toElement($(anchor));
+
+    });
+
+
+});
