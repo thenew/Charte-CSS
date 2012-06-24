1d7ff62bbb5c44dac75cbdbd5feaef057f4270f0
thenew <remy@thenew.fr>
2012-06-25 00:12:12 +0200

    formatage avec notes en js

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,21 +1,30 @@
-
-
 window.addEvent('domready', function(){
 
     // Highlight
-    hljs.initHighlightingOnLoad();
+    $$('.Highlight').each(function(el){
+        hljs.highlightBlock(el)
+    });
+
+    // notes
+    $$('.n').addEvents({
+        mouseenter: function(e){
+            console.log(e.target.getProperty('data-target'));
+            var def = e.target.getProperty('data-target');
+            $$('#'+def).toggleClass('active');
+        },
+        mouseleave: function(e){
+            var def = e.target.getProperty('data-target');
+            $$('#'+def).toggleClass('active');
+        }
+    });
 
     // Smooth scroll
     var scrollAnchor = new Fx.Scroll(window);
-
     $$('.scrollAnchor').addEvent('click', function(e){
         e.preventDefault();
-
         var href = e.target.getProperty('href');
         var anchor = href.split('#')[1];
-
         scrollAnchor.toElement($(anchor));
-
     });
 
 
